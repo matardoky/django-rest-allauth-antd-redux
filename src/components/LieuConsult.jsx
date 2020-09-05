f4c7430baa8b3155636b5772fb43f67144ec2de0
MatarDoky <mdokydiop@gmail.com>
2020-09-05 02:40:58 +0200

    LieuConsult component

--- src/components/LieuConsult.jsx
+++ src/components/LieuConsult.jsx
@@ -1,10 +1,28 @@
 import React from 'react' 
+import { PageHeader, Alert, Row, Col } from 'antd'
+import ListLieuConsult from '../containers/ListLieuConsult'
+import DetailLieuConsult from '../containers/DetailLieuConsult'
 
 
 class LieuConsult extends React.Component {
     render(){
+        const description = "Renseignez er gérez toutes les informations relatives à votre lieu de consultation. Ces derniéres permettent à vos patients de vous trouver plus simplement"
         return ( 
-            <p>Information</p>
+            <PageHeader
+            title="Lieu de consultation"
+            >
+                <Alert type="info" message ={description} />
+                <Row>
+                    <Col span={8}>
+                        <ListLieuConsult/>
+                    </Col>
+
+                    <Col span={16}>
+                        <DetailLieuConsult/>
+                    </Col>
+                </Row>
+                
+            </PageHeader>
         )
     }
 }
---
+++ src/containers/ListLieuConsult.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ListLieuConsult extends React.Component {
+    render(){
+        return (
+            <div>List</div>
+        )
+    }
+}
+
+export default ListLieuConsult
